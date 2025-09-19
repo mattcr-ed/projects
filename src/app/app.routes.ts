f8158c79cb1a7b7881a1658ecf9437a742de7620
Matt C-R <595637+mattcr-ed@users.noreply.github.com>
2025-09-19 10:14:43 +0100

    Basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Homepage } from './homepage/homepage';
+import { Picalc } from './picalc/picalc';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Homepage,
+    },
+    {
+        path: 'picalc',
+        component: Picalc,
+    },
+];
---
+++ src/app/picalc/picalc.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-picalc',
+  imports: [],
+  templateUrl: './picalc.html',
+  styleUrl: './picalc.css'
+})
+export class Picalc {
+
+}
