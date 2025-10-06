b48ef3364a7b54f8fbca731d811a04d8694e7361
Matt C-R <595637+mattcr-ed@users.noreply.github.com>
2025-10-06 19:10:15 +0100

    ToDo list boilerplate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { Picalc } from './pages/maths/picalc/picalc';
 import { Homepage } from './pages/homepage/homepage';
 import { WeatherChecker } from './pages/misc/weather-checker/weather-checker';
+import { Todolist } from './pages/misc/todolist/todolist';
 
 export const routes: Routes = [
     {
@@ -16,5 +17,9 @@ export const routes: Routes = [
     {
         path: 'misc/weatherchecker',
         component: WeatherChecker
+    },
+    {
+        path: 'misc/todolist',
+        component: Todolist
     },
 ];
---
+++ src/app/pages/misc/todolist/todolist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  imports: [],
+  templateUrl: './todolist.html',
+  styleUrl: './todolist.css'
+})
+export class Todolist {
+
+}
